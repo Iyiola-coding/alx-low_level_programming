09027bc0f57a21ee3c5330e7486f413282bc4a4d
iyitech <roberts@gmail.com>
2023-10-20 05:18:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - A programe that print a line using printf function
+ *  Return: 0 (Perfect)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
