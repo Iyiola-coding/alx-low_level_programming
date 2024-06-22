106312e89f7a5cffff5f69dc44f6c44a7abb6fc0
Iyiola-coding <iyiolache@gmail.com>
2024-06-21 20:20:12 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int h = '0';
+	int k = '0';
+
+	while (h <= '9')
+	{
+		while (k <= '9')
+		{
+			if (!(h > k) || h == k)
+			{
+
+				putchar(h);
+				putchar(k);
+				if (h == '8' && k == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			k++;
+		}
+		k = '0';
+	}
+return (0);
+}
