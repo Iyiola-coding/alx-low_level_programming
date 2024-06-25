6e1423731200630e50b659ae55194cff14170efe
Iyiola-coding <iyiolache@gmail.com>
2024-06-25 16:24:20 -0700

    4-isalpha.c. git push git add . ls

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character 
+ * _isalpha - checks for alphabetic character
  * @c: To be checked
  * Return: 1 for alphabetic character or 0 fior otherwise
  */
