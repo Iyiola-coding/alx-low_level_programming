5c8e14a67a1b99ddb3d0416073337a69b589d857
Iyiola-coding <iyiolache@gmail.com>
2024-06-25 12:56:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Starting point, print_alphabet = alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char r;
+for (r = 'a'; r <= 'z'; r++)
+_putchar(r);
+_putchar('\n');
+}
