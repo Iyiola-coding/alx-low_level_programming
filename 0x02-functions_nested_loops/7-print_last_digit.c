43c495193a36db961f489f315f286dfb0765d43c
Iyiola-coding <iyiolache@gmail.com>
2024-06-25 18:39:18 -0700

    #include "main.h"
    /**
     * print_last_digit
     * @n: Numbers to be treated
     * Return: Value of the last digit of numbers
     */
    
    int print_last_digit(int n)
    {
            int last;
    
            last = n % 10;
            if (last < 0)
            {
            last = last * -1;
            }
            _putchar(last + 0);

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,20 +2,18 @@
 
 /**
  * print_last_digit 
- * @n: Number to be treated
- * Return: Value of the last digit of number
+ * @n: Numbers to be treated
+ * Return: Value of the last digit of numbers
  */
 
 int print_last_digit(int n)
-
 {
-	int last;
+        int last;
 
-	last = n % 10;
-	if (last < 0)
-	{
-	last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
-}
+        last = n % 10;
+        if (last < 0)
+        {
+        last = last * -1;
+        }
+        _putchar(last + '0');
+        return (last)
