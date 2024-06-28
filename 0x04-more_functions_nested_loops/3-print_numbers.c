1456d573834d98b85538e18fd50f6e62abba00d4
Iyiola-coding <iyiolache@gmail.com>
2024-06-27 20:45:51 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: no
+ */
+
+void print_numbers(void)
+{
+	int pr;
+
+	for (pr = 0; pr <= 9; pr++)
+	{
+		_putchar(pr);
+	}
+	_putchar('\n');
+}
