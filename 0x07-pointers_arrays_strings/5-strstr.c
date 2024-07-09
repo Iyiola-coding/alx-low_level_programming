d59dfce643e74aa193532088ce830457857d1c00
Iyiola-coding <iyiolache@gmail.com>
2024-07-08 21:30:17 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strstr - function name
- * @
+ * @haystack: input one
+ * @needle: input two
+ * Return: 0 eachtime
+ */
+
 char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack)
+	}
+	return (NULL);
+}
