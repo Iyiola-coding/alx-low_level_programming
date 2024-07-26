a507f48e226b0fb7481364587cd6303622f400f3
Iyiola-coding <iyiolache@gmail.com>
2024-07-25 18:04:27 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _variagic_functions_H
+#define _variagic_functions_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
+
